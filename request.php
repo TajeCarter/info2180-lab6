98b8d064441a5cf93b1d2078b5cf0396a34aa3b6
Taje Carter <Taje Carter>
2016-11-09 13:53:19 -0500

    Exercise 3.

---
+++ request.php
@@ -0,0 +1,34 @@
+<?php
+// accept a term (keyword)
+// respond with a value
+$query = $_GET['q'];
+$all = $_GET['all'];
+
+$definition = [
+    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
+    "bar" => "A place that sells alcholic beverages",
+    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
+    "html" => "The standard markup language for creating web pages and web applications.",
+    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
+    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
+    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
+];
+
+if($all==true and $query==null){
+    header("Content-type: text/xml");
+    
+    $str = '<entries>'; //xml - open tag
+    foreach($definitions as $key=>$value){
+        $str.='<definition name = \''.$key.'\'>'.$value.'</definition>';
+    }
+
+     $str.='</entries>'; //xml - close tag
+
+      $xml = new SimpleXMLElement($str);
+    echo $xml->asXML();
+}
+else{
+	print "<h3>" . strtoupper($query) . "</h3>";
+	print "<p>" . $definition[$query] . "</p>";
+}
+?>
--- urban.js
+++ urban.js
@@ -11,7 +11,44 @@ function OnLookUpButtonClicked(event)
 					{
 						method:"get",
 						onSuccess:ShowResult,
-						onFailure: ajaxFailure {alert('Something went wrong...'); }
+						onFailure: ajaxFailure
+						parameters : {
+							"term": $("term").value,
+							"format": "xml"
+
+									}
 					}
 					);
+}
+
+function gotResult(ajax) {
+	var root = ajax.responseXML.firstChild;
+	var error = root.getAttribute("error");
+	
+	if (error) {
+		$("result").innerHTML = error;
+	} else {
+		var definition = document.createElement("p");
+		var example = document.createElement("p")
+		var author = document.createElement("p");
+		example.className = "example";
+	
+		var entry = root.getElementsByTagName("entry")[0];
+		var definitionNode = entry.getElementsByTagName("definition")[0];
+		definition.innerHTML = definitionNode.firstChild.nodeValue;
+
+		var exampleNode = entry.getElementsByTagName("example")[0];
+		example.innerHTML = exampleNode.firstChild.nodeValue;
+		author.innerHTML = "- " + entry.getAttribute("author");
+	
+		$("result").innerHTML = "";
+		$("result").appendChild(definition);
+		$("result").appendChild(example);
+		$("result").appendChild(author);
+	}
+
+
+	function ajaxFailure(ajax)
+{
+	alert(ajax.status + " " + ajax.statusText);
 }
